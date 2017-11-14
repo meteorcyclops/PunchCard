4dd3275110ff9855a532dd185891987148617429
meteorcyclops <meteorcyclops1104@gmail.com>
2017-11-14 10:58:14 +0800

    桌面全寬版本

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 // import green from 'material-ui/colors/green'
 
 import BookCardBody from './BookCardBody'
@@ -10,23 +10,25 @@ import './css/App.css'
 class App extends Component {
     render() {
         return (
-            <div className="login-wrap">
-                <div className="login-html">
-                    <input id="tab-1" type="radio" name="tab" className="sign-in" defaultChecked  />
-                    <label htmlFor="tab-1" className="tab">上班</label>
-                    <input id="tab-2" type="radio" name="tab" className="sign-up" />
-                    <label htmlFor="tab-2" className="tab">下班</label>
+            <div className="for-the-overlay">
+                <div className="login-wrap">
+                    <div className="login-html">
+                        <input id="tab-1" type="radio" name="tab" className="sign-in" defaultChecked/>
+                        <label htmlFor="tab-1" className="tab">上班</label>
+                        <input id="tab-2" type="radio" name="tab" className="sign-up"/>
+                        <label htmlFor="tab-2" className="tab">下班</label>
 
-                    <div className="login-form" style={{marginTop: '8%'}}>
-                        <BookCardBody
-                            className="sign-in-htm"
-                            onClick={bookStore.onBoard}
-                            in
-                        />
-                        <BookCardBody
-                            className="sign-up-htm"
-                            onClick={bookStore.offBoard}
-                        />
+                        <div className="login-form" style={{marginTop: '8%'}}>
+                            <BookCardBody
+                                className="sign-in-htm"
+                                onClick={bookStore.onBoard}
+                                in
+                            />
+                            <BookCardBody
+                                className="sign-up-htm"
+                                onClick={bookStore.offBoard}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
