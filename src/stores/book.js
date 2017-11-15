ba5306b4a23d5eb5dc2edbc2d3c3d05a0a3fe992
tao <flyingpath@gmail.com>
2017-11-15 12:01:09 +0800

    update

--- src/stores/book.js
+++ src/stores/book.js
@@ -3,43 +3,45 @@ import {observable, action} from 'mobx'
 
 import writeBook from './writeBook'
 
-let rawBackendTime = moment()
+class BookStore {
+    @observable
+    uid= localStorage.getItem('uid') || ""
+    @observable
+    pwd= localStorage.getItem('pwd') || ""
+    @observable
+    locked= localStorage.getItem('locked')=='true' || false
+    @observable
+    status= ""
+    @observable
+    emergency=  false
+    @observable
+    backendTime= moment().format('hhmmss')
 
-const bookStore = observable({
-    uid: localStorage.getItem('uid') || "",
-    pwd: localStorage.getItem('pwd') || "",
-    locked: localStorage.getItem('locked')=='true' || false,
-    status: "",
-    emergency:  false,
-    backendTime: moment().format('hhmmss'),
+    @action
+    setObs(key, value){
+        this[key] = value
+    }
+    
+    @action
+    getBackendTime(){
+        const uri = 'https://gateway.kfsyscc.org/Gateway/a/CardClient/ClockTimeStamp'
+        fetch(uri)
+        .then( res=>res.json() )
+        .then( backdata=>{
+            if(backdata.TimeStamp){
+                this.rawBackendTime = moment(backdata.TimeStamp)
+                setInterval(
+                    ()=>{
+                        this.rawBackendTime = this.rawBackendTime.add(1, 'seconds')
+                        this.setObs('backendTime', this.rawBackendTime.format('hhmmss'))
+                    },
+                    1000
+                )
+            }
+        } )
+    }
 
-    setObs: action(
-        (key, value)=>{
-            bookStore[key] = value
-        }
-    ),
-
-    getBackendTime: action(
-        ()=>{
-            const uri = 'https://gateway.kfsyscc.org/Gateway/a/CardClient/ClockTimeStamp'
-            fetch(uri)
-            .then( res=>res.json() )
-            .then( backdata=>{
-                if(backdata.TimeStamp){
-                    rawBackendTime = moment(backdata.TimeStamp)
-                    setInterval(
-                        ()=>{
-                            rawBackendTime = rawBackendTime.add(1, 'seconds')
-                            bookStore.setObs('backendTime', rawBackendTime.format('hhmmss'))
-                        },
-                        1000
-                    )
-                }
-            } )
-        }
-    ),
-
-    check: (type) => {
+    check = (type) => {
         writeBook(type, bookStore.uid, bookStore.pwd).then((res) => {
             let status = res.ErrorMessage
             if (status === "") {
@@ -59,27 +61,32 @@ const bookStore = observable({
                     status = `${username} 下班打卡成功(${dt})`
                 }
 
-                localStorage.setItem('uid', bookStore.uid)
-                localStorage.setItem('pwd', bookStore.pwd)
-                localStorage.setItem('locked', bookStore.locked)
+                localStorage.setItem('uid', this.uid)
+                localStorage.setItem('pwd', this.pwd)
+                localStorage.setItem('locked', this.locked)
             }   
-            bookStore.setObs('status', status)
+            this.setObs('status', status)
         })
-    },
-    onBoard: () => {
-        if (bookStore.emergency) {
-            bookStore.check(2);
+    }
+    onBoard= () => {
+        if (this.emergency) {
+            this.check(2);
         } else {
-            bookStore.check(1);
-        }
-    },
-    offBoard: () => {
-        if (bookStore.emergency) {
-            bookStore.check(3);
-        } else {
-            bookStore.check(9);
+            this.check(1);
         }
     }
-})
+    offBoard= () => {
+        if (this.emergency) {
+            this.check(3);
+        } else {
+            this.check(9);
+        }
+    }
+
+    rawBackendTime = moment()
+}
+
+var bookStore = new BookStore()
+
 export default bookStore
 
