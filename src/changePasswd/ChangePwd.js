70f1a890a1abf5fb91e3902dcab42d24a1774298
丁丁 <advancedor96@gmail.com>
2018-04-13 16:27:40 +0800

    主畫面完成

--- src/changePasswd/ChangePwd.js
+++ src/changePasswd/ChangePwd.js
@@ -7,57 +7,77 @@ import Paper from 'material-ui/Paper';
 import Typography from 'material-ui/Typography';
 
 import bookStore from '../stores/book';
+import FontAwesome from 'react-fontawesome';
 
-const styles = {
-   paper: {
-      position:'absolute',
-      top:'10px',
-      bottom:'10px',
-      left:'10px',
-      right:'10px',
-      zIndex: '4',
-   }
-}
+
 class ChangePwd extends Component {
-   constructor(props) {
-      super(props);
-      this.state = {
-          username: bookStore.uid,
-          password: bookStore.pwd,
-      }
-  }
-  handleSend = ()=>{
-   changePasswdStore.sendToServer(this.adAccount.value, this.oldPasswd.value, this.newPasswd.value);
-  }
-  
-  togglePasswd = (target)=>{
-   if(target.getAttribute("type")==='password'){
-      target.setAttribute('type', 'text');
-   }else{
-      target.setAttribute('type', 'password');
-   }
-  }
-   render() {
-      if (changePasswdStore.openPwdChanging) {
-         return (
-            <Paper elevation={4} style={styles.paper}>
-               <h1>修改密碼</h1>
-               帳號：<input type='text' value={bookStore.uid} ref={a => this.adAccount = a}/><br />
-               舊密碼：<input type='text' ref={a => this.oldPasswd = a}/><button onClick={()=>{this.togglePasswd(this.oldPasswd)}}>切換</button><br />
-               新密碼：<input type='text'  ref={a => this.newPasswd = a}/><button onClick={()=>{this.togglePasswd(this.newPasswd)}}>切換</button><br />
-               {/* 新密碼：<input type='text'  ref={a => this.newPasswdAgain = a}/><br /> */}
-               <button onClick={this.handleSend}>送出</button>
-               <button onClick={()=>{changePasswdStore.setPwdOpen(false);}}>關閉</button>
-            </Paper>
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: bookStore.uid,
+            password: bookStore.pwd,
+        }
+    }
+    handleSend = () => {
+        changePasswdStore.sendToServer(this.adAccount.value, this.oldPasswd.value, this.newPasswd.value);
+    }
+
+    togglePasswd = (target) => {
+        if (target.getAttribute("type") === 'password') {
+            target.setAttribute('type', 'text');
+        } else {
+            target.setAttribute('type', 'password');
+        }
+    }
+    render() {
+        if (changePasswdStore.openPwdChanging) {
+            return (
+                <Paper elevation={4} className="changePwdPanel">
+                    <div className="pwdPanel_wrapper">
+                        <h1 className="pwdPanel_title">修改密碼</h1>
+
+                        <div className="pwdPanel_field">帳號</div>
+                        <div className="pwdPanel_row">
+                            <input type='text' className="pwdPanel_input" value={bookStore.uid} ref={a => this.adAccount = a} />
+                        </div>
+
+                        <div className="pwdPanel_field">原密碼</div>
+                        <div className="pwdPanel_row">
+                            <input type='text' className="pwdPanel_input" ref={a => this.oldPasswd = a} />
+                            <FontAwesome name='eye-slash'
+                                size='2x'
+                                style={{ color: 'rgba(255, 255, 255, 0.7)', cursor: 'pointer', position: 'absolute', right: '18px', padding: '1px 10px' }}
+                                onClick={() => { this.togglePasswd(this.oldPasswd) }}
+                            />
+                        </div>
 
 
-         );
-      } else {
-         return null;
-      }
+                        <div className="pwdPanel_field">新密碼</div>
+                        <div className="pwdPanel_row">
+                            <input type='text' className="pwdPanel_input" ref={a => this.newPasswd = a} />
+                            <FontAwesome name='eye-slash'
+                                size='2x'
+                                style={{ color: 'rgba(255, 255, 255, 0.7)', cursor: 'pointer', position: 'absolute', right: '18px', padding: '1px 10px' }}
+                                onClick={() => { this.togglePasswd(this.newPasswd) }}
+                            />
+                        </div>
+
+                        {/* 新密碼：<input type='text'  ref={a => this.newPasswdAgain = a}/><br /> */}
+                        <div className="pwdPanel_row" style={{ justifyContent: 'space-between' }}>
+                            <button className="pwdPanel_submit" onClick={this.handleSend}>送出</button>
+                            <button className="pwdPanel_cancel" onClick={() => { changePasswdStore.setPwdOpen(false); }}>放棄</button>
+                        </div>
+                    </div>
+                </Paper>
 
 
-   }
+            );
+        } else {
+            return null;
+        }
+
+
+    }
 }
 
 export default observer(ChangePwd);
