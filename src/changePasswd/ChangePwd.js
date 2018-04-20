a0df9ed10cb01d1b612212f9e4c1aac660dd4c7c
丁丁 <advancedor96@gmail.com>
2018-04-20 16:17:45 +0800

    找問題

--- src/changePasswd/ChangePwd.js
+++ src/changePasswd/ChangePwd.js
@@ -14,122 +14,155 @@ class ChangePwd extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            username: bookStore.uid,
-            password: bookStore.pwd,
+            // username: bookStore.uid  ,  /**************** */
+            // password: bookStore.pwd,
             err_msg: '',
+            show_pwd_again_not_same: false,
+
             origin_pwd_show: false,
             new_pwd_show: false,
+            new_pwd_again_show: false,
         }
+
+        this.adAccount = ''
+        this.oldPasswd = ''
+        this.newPasswd = ''
+        this.newPasswdAgain = ''
     }
+
     handleSend = () => {
-        if(this.adAccount.value.length === 0  || 
-            this.oldPasswd.value.length === 0 || 
-            this.newPasswd.value.length === 0 ||
-            this.checkNewPasswd(this.newPasswd.value) === false
-        ){
+        if (
+            this.adAccount || this.oldPasswd || this.newPasswd || this.checkNewPasswd( this.newPasswd ) === false
+        ) {
+            return false;
+        }
+        console.log('this.newPasswdAgain:',this.newPasswdAgain);
+        console.log('this.newPasswd:',this.newPasswd);
+        if( this.newPasswdAgain !== this.newPasswd){
+            this.setState({show_pwd_again_not_same: true});
             return false;
         }
 
         changePasswdStore.sendToServer(this.adAccount.value, this.oldPasswd.value, this.newPasswd.value);
     }
 
-    checkNewPasswd= (value)=>{
+    onChangeInput( e, key ){
+        this[key] = e.target.value;
+        if(key === 'newPasswd'){
+            this.checkNewPasswd(this[key]);
+        }
+    }
+
+    checkNewPasswd = (value) => {
         const errChar = ' ';
         const mypwd = value;
-        let isOK = true;
+        
         let err_msg_list = [];
 
-        if(mypwd.length < 5){
+        if (mypwd.length < 5) {
             err_msg_list.push('至少5個字元');
-            isOK = false;
         }
 
-        for(let i=0;i<mypwd.length;i++){
-            if( _.includes(errChar, mypwd[i]) ){
+        for (let i = 0; i < mypwd.length; i++) {
+            if (_.includes(errChar, mypwd[i])) {
                 err_msg_list.push('不得使用<空格>');
-                isOK = false;
                 break;
             }
         }
 
-
-        if(isOK){
-            this.setState({err_msg: ''});
-            return isOK;
-        }else{
-            this.setState({err_msg: err_msg_list.join(', ')  });
-            return isOK;
+        if ( err_msg_list.length === 0 ) {
+            this.setState({ err_msg: '' });
+            return true;
+        
+        } else {
+            this.setState({ err_msg: err_msg_list.join(', ') });
+            return false;
         }
     }
+
     render() {
-        if (changePasswdStore.openPwdChanging) {
-            return (
-                <Paper elevation={4} className="changePwdPanel">
-                    <div className="pwdPanel_wrapper">
-                        {/* <div className="pwdPanel_row"> */}
-                            <FontAwesome name='times-circle' 
-                                size='lg' 
-                                style={{ color: 'white', cursor: 'pointer', position:'absolute', top:'23px', left:'23px' }} 
-                                onClick={() => { changePasswdStore.setPwdOpen(false); }}
-                            />
-                            <h1 className="pwdPanel_title">修改密碼</h1>
-                        {/* </div> */}
-
-                        <div className="pwdPanel_field">帳號</div>
-                        <div className="pwdPanel_row">
-                            <input type='text' className="pwdPanel_input" defaultValue={bookStore.uid} ref={a => this.adAccount = a} />
-                        </div>
-
-                        <div className="pwdPanel_field">原密碼</div>
-                        <div className="pwdPanel_row">
-                            <input type={this.state.origin_pwd_show? 'text' : 'password'} className="pwdPanel_input" ref={a => this.oldPasswd = a} />
-                            <span 
-                                onClick={() => {this.setState( (prevState)=>{return {origin_pwd_show: !prevState.origin_pwd_show}}   ) }}
-                                style={{ color: 'rgba(255, 255, 255, 0.7)', cursor: 'pointer', position: 'absolute', right: '18px', padding: '1px 10px', fontWeight: '100' }}
-                            >{this.state.origin_pwd_show? '隱藏':'顯示'}</span>
-
-                        </div>
-
-
-                        <div className="pwdPanel_field">新密碼
-                            <sup><FontAwesome name='info-circle'
-                                size='lg'
-                                style={{ color: 'rgba(255, 255, 255, 0.7)', cursor: 'pointer', fontSize:'0.933333em', marginLeft:'8px'}}
-                                onClick={()=>{ swal('密碼原則', `1. 至少 5個字元
-                                2. 英文大寫字元 (A 到 Z)
-                                3. 英文小寫字元 (a 到 z) 
-                                4. 10 進位數字 (0 到 9) 
-                                `); }}
-                                title="密碼原則"
-                            /></sup>
-
-                            <span className="pwdPanel_pwdHint">{this.state.err_msg}</span>
-                        </div>
-                        <div className="pwdPanel_row">
-                            <input type={this.state.new_pwd_show? 'text' : 'password'}
-                                className="pwdPanel_input" 
-                                ref={a => this.newPasswd = a} 
-                                onChange={()=>{this.checkNewPasswd(this.newPasswd.value)}}/>
-                            <span 
-                                onClick={() => {this.setState( (prevState)=>{return {new_pwd_show: !prevState.new_pwd_show}}   ) }}
-                                style={{ color: 'rgba(255, 255, 255, 0.7)', cursor: 'pointer', position: 'absolute', right: '18px', padding: '1px 10px', fontWeight: '100' }}
-                            >{this.state.new_pwd_show? '隱藏':'顯示'}</span>
-
-                        </div>
-
-                        {/* 新密碼：<input type='text'  ref={a => this.newPasswdAgain = a}/><br /> */}
-                        <div className="pwdPanel_row" style={{ justifyContent: 'space-between' }}>
-                            <button className="pwdPanel_submit" onClick={this.handleSend}>送出</button>
-                            <button className="pwdPanel_cancel" onClick={() => { changePasswdStore.setPwdOpen(false); }}>放棄</button>
-                        </div>
-                    </div>
-                </Paper>
-
-
-            );
-        } else {
+        
+        if (changePasswdStore.openPwdChanging === false) {
             return null;
         }
+        
+        return (
+            <Paper elevation={4} className="changePwdPanel">
+                <div className="pwdPanel_wrapper">
+                    <FontAwesome name='times-circle'
+                        size='lg'
+                        style={{ color: 'white', cursor: 'pointer', position: 'absolute', top: '23px', left: '23px' }}
+                        onClick={() => { changePasswdStore.setPwdOpen(false); }}
+                    />
+                    <h1 className="pwdPanel_title">修改密碼</h1>
+
+                    <div className="pwdPanel_field">帳號</div>
+                    <div className="pwdPanel_row">
+                        <input type='text' defaultValue={bookStore.uid} onChange={ (e)=>{ this.onChangeInput(e, 'adAccount') }  } />
+                    </div>
+
+                    <div className="pwdPanel_field">原密碼</div>
+                    <div className="pwdPanel_row">
+                        <input type={this.state.origin_pwd_show ? 'text' : 'password'} onChange={ (e)=>{ this.onChangeInput(e, 'oldPasswd') }} />
+                        <span 
+                            className="pwdPanel_showOrHide"
+                            onClick={ () => { this.setState((prevState) => { return { origin_pwd_show: !prevState.origin_pwd_show } }) }}
+                        >{this.state.origin_pwd_show ? '隱藏' : '顯示'}</span>
+
+                    </div>
+
+
+                    <div className="pwdPanel_field">新密碼
+                        <FontAwesome name='info-circle'
+                            className="pwdPanel_info"
+                            size='lg'
+                            onClick={() => {
+                                swal('密碼原則', `1. 至少 5個字元
+                            2. 英文大寫字元 (A 到 Z)
+                            3. 英文小寫字元 (a 到 z) 
+                            4. 10 進位數字 (0 到 9) 
+                            `);
+                            }}
+                            title="密碼原則"
+                        />
+
+                        <span className="pwdPanel_pwdHint">{this.state.err_msg}</span>
+                    </div>
+                    <div className="pwdPanel_row">
+                        <input type={this.state.new_pwd_show ? 'text' : 'password'}
+                            onChange={ (e)=>{ this.onChangeInput(e, 'newPasswd') }} 
+                        />
+
+                        <span className="pwdPanel_showOrHide"
+                            onClick={() => { this.setState((prevState) => { return { new_pwd_show: !prevState.new_pwd_show } }) }}
+                        >{this.state.new_pwd_show ? '隱藏' : '顯示'}</span>
+
+                    </div>
+
+                    <div className="pwdPanel_field">確認新密碼
+                        <span className="pwdPanel_pwdHint">{this.state.show_pwd_again_not_same? '兩次密碼不一致' :''}</span>
+                    </div>
+                    <div className="pwdPanel_row">
+                        <input type={this.state.new_pwd_again_show ? 'text' : 'password'}
+                            onChange={ (e)=>{ this.onChangeInput(e, 'newPasswdAgain') }} 
+                        />
+
+                        <span className="pwdPanel_showOrHide"
+                            onClick={() => { this.setState((prevState) => { return { new_pwd_again_show: !prevState.new_pwd_again_show } }) }}
+                        >{this.state.new_pwd_again_show ? '隱藏' : '顯示'}</span>
+
+                    </div>
+
+                    <div className="pwdPanel_row" style={{ justifyContent: 'space-between' }}>
+                        <button className="pwdPanel_submit" onClick={this.handleSend}>送出</button>
+                        <button className="pwdPanel_cancel" onClick={() => { changePasswdStore.setPwdOpen(false); }}>放棄</button>
+                    </div>
+                </div>
+            </Paper>
+
+
+        );
+
 
 
     }
