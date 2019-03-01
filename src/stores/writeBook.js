32709e16639bfc27f0522da9d641a7fb5ff8565b
HongJhe <hongjhe@kfsyscc.org>
2019-03-01 14:00:43 +0800

    符合資格自動申請加班

--- src/stores/writeBook.js
+++ src/stores/writeBook.js
@@ -1,7 +1,7 @@
-import moment from 'moment'
+import _ from 'lodash'
+import swal from 'sweetalert'
 
 import bookStore from './book'
-import axios from 'axios';
 import changePasswdStore from './ChangePasswdStore';
 
 const url = "https://staff.kfsyscc.org/hrapi/card/" //https://staff.kfsyscc.org/hrapi/card/
@@ -37,6 +37,10 @@ const writeBook = (cardtype, username, password) => {
         let result = res
         if (result.status){
             bookStore.setObs('status', result.msg)
+
+            if (cardtype == '9') { // 打下班卡才需判斷
+                checkOvertimeOrHasNightFee(username, password)
+            }
         }else{
             bookStore.setObs('status', result.err)
         }
@@ -64,6 +68,139 @@ const writeBook = (cardtype, username, password) => {
             4. 以上都沒用，記下狀況後聯絡資訊部王傳道#3466`)
         }
     })
+}
+
+const checkOvertimeOrHasNightFee = (username, password) => {
+    // console.log('username, password', username, password)
+    // console.log('bookStore.uid', bookStore.uid)
+
+    return fetch(url, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify({
+            api: 'checkOvertimeOrHasNightFee',
+            username: `${username}`,
+            password: `${password}`
+        })
+    }).then((res) => {
+        return res.json()
+    }).then((res) => {
+        // res = {
+        //     "emp_no": emp_no,
+        //     "on_time": on_time,
+        //     "off_time": off_time,
+        //     "ot_hours": ot_hours,
+        // }     
+        console.log(res)
+        const { status, data } = res
+        if (status) {
+            const { emp_no, on_time, off_time, ot_hours } = data
+
+            if (ot_hours >= 2) {
+                swal({
+                    title: '請選擇加班費或積休',
+                    text: '您今日延長工時預設加班是報加班費，如須積休請自行選擇。',
+                    icon: 'warning',
+                    buttons: {
+                        dayoff: '積休',
+                        money: '加班費'
+                    },
+                    dangerMode: true,
+                }).then((option) => {
+                    // console.log('option', option)
+                    switch (option) {
+                        case 'dayoff':
+                            swal('你選擇「積休」', {
+                                icon: 'success',
+                            })
+                            break
+
+                        default:
+                            swal('你選擇「加班費」', {
+                                icon: 'success',
+                            })
+                            break
+                    }
+
+                    sendToFlow(option, data)
+                })
+            }
+        }
+    })
+}
+
+const sendToFlow = (type, data) => {
+    const { emp_no, boss_id, on_time, off_time, ot_time, ot_hours } = data
+    let salaryWeekdays = '' // 平日 計薪
+    let accumulationWeekdays = '' // 平日 積假
+    let typeName = '「加班費」'
+
+    if (type == 'dayoff') {
+        accumulationWeekdays = ot_hours
+        typeName = '「積休」'
+    } else {
+        salaryWeekdays = ot_hours
+    }
+
+    const user_id = emp_no
+    const startDate = off_time
+    const endDate = ot_time
+    const add_thingsay = '新排班測試'
+    // const salaryWeekdays = '' // 平日 計薪
+    const salaryRest = ''
+    const salaryEmptyClass = ''
+    const salaryTeacherTraining = ''
+    const salaryReward = ''
+    const salaryCountrySetting = ''
+    const salarySpecialBreak = ''
+    const salaryTyphoon = ''
+    const salarySpecialOvertime = ''
+    const salarySpecialOvertimeInHoliday = ''
+    const salarySpecialOvertimeInEvening = ''
+    const salaryDistance = ''
+    // const accumulationWeekdays = '' // 平日 積假
+    const accumulationRest = ''
+    const accumulationEmptyClass = ''
+    const accumulationTeacherTraining = ''
+    const accumulationReward = ''
+    const accumulationCountrySetting = ''
+    const accumulationSpecialBreak = ''
+    const accumulationTyphoon = ''
+    const accumulationSpecialOvertime = ''
+    const accumulationSpecialOvertimeInHoliday = ''
+    const accumulationSpecialOvertimeInEvening = ''
+    const accumulationDistance = ''
+    const sendData = `user_id=${user_id}&startDate=${startDate}&endDate=${endDate}&boss_id=${boss_id}&add_thingsay=${add_thingsay}&salaryWeekdays=${salaryWeekdays}&salaryRest=${salaryRest}&salaryEmptyClass=${salaryEmptyClass}&salaryTeacherTraining=${salaryTeacherTraining}&salaryReward=${salaryReward}&salaryCountrySetting=${salaryCountrySetting}&salarySpecialBreak=${salarySpecialBreak}&salaryTyphoon${salaryTyphoon}=&salarySpecialOvertime=${salarySpecialOvertime}&salarySpecialOvertimeInHoliday=${salarySpecialOvertimeInHoliday}&salarySpecialOvertimeInEvening=${salarySpecialOvertimeInEvening}&salaryDistance=${salaryDistance}&accumulationWeekdays=${accumulationWeekdays}&accumulationRest=${accumulationRest}&accumulationEmptyClass=${accumulationEmptyClass}&accumulationTeacherTraining=${accumulationTeacherTraining}&accumulationReward=${accumulationReward}&accumulationCountrySetting=${accumulationCountrySetting}&accumulationSpecialBreak=${accumulationSpecialBreak}&accumulationTyphoon=${accumulationTyphoon}&accumulationSpecialOvertime=${accumulationSpecialOvertime}&accumulationSpecialOvertimeInHoliday=${accumulationSpecialOvertimeInHoliday}&accumulationSpecialOvertimeInEvening=${accumulationSpecialOvertimeInEvening}&accumulationDistance=${accumulationDistance}`
+    const host = 'flow-test.kfcc.intra'
+    // 正式區網址： flow.kfsyscc.org 測試區網址： flow-test.kfcc.intra
+
+    fetch(`http://${host}/flow/MyFlowWS/MyFlowWebService.asmx/sendWorkOvertime?${sendData}`)
+        .then(res => {
+            return res.text()
+        })
+        .then(req => {
+            console.warn(req)
+            // <?xml version="1.0" encoding="utf-8"?><string xmlns="http://tempuri.org">{"status":false,"msg":"您申請的加班時間與之前記錄重疊！","data":{}}</string>
+            req = req.replace(`<?xml version="1.0" encoding="utf-8"?>`, '')
+                .replace(`<string xmlns="http://${host}/MyFlowWebService">`, '')
+                .replace(`<string xmlns="http://tempuri.org">`, '')
+                .replace(`</string>`, '')
+                .trim()
+            req = JSON.parse(req)
+            // console.log('req', req)
+            const form_id = _.get(req, ['data', 'form_id'])
+            if (req['status'] && !isNaN(parseInt(form_id)) && form_id.length === 10) {
+                swal(`${typeName} 表單已送出！`, {
+                    icon: "success",
+                });
+                // console.log(req)    
+            } else {
+                swal(req['msg'], {
+                    icon: "warning",
+                });
+                // console.warn(req['msg'])
+            }
+        }).catch(err => console.log(err))
 }
 
 export default writeBook
