0264d24844e488b59e53f6ff01d1d634fe5420c0
Tao <flyingpath@gmail.com>
2019-04-19 23:50:47 +0800

    update for demo

---
+++ doc/sw.js
@@ -0,0 +1,85 @@
+// // install
+self.addEventListener('install', function (event) {
+    console.log('sw installed')
+    self.skipWaiting();
+    // event.waitUntil(
+    //   caches.open('KfsysccCard').then(function(cache) {
+    //     return cache.addAll([
+    //       './',
+    //     ]);
+    //   })
+    // );
+});
+
+// fetch
+self.addEventListener('fetch', function (event) {
+  if (
+    event.request.url.indexOf('hrapi') === -1
+  ){
+    if (
+      event.request.url === 'https://staff.kfsyscc.org/card/' 
+      || event.request.url ==='https://staff.kfsyscc.org/card/index.html' 
+      || event.request.url ==='http://localhost:3000/' 
+      // || event.request.url.indexOf('sw') > -1
+    ){
+      event.respondWith(
+        fetch(event.request)
+          .then( function(res){
+            if(res.status === 200) {
+              return (
+                caches.open( 'KfsysccCard' )
+                .then( function(cache) {
+                  cache.put( event.request.url, res.clone() );
+                  return res
+                } )
+              )
+            } else {
+              return caches.match(event.request).then( function (response){
+                if (response) {
+                  return response;
+                } else {
+                    return res
+                }
+              })
+            }
+        })
+        .catch (
+          e => {
+            return caches.match(event.request).then( function (response){
+              if (response) {
+                return response;
+              } else {
+                return e
+              }
+            })
+          }
+        ) 
+      );
+    } else {
+      event.respondWith(
+        caches.match(event.request).then( function (response){
+          if (response) {
+            return response;
+          } else{
+            return (
+              fetch(event.request)
+              .then( function(res){
+                return (
+                  caches.open( 'KfsysccCard' )
+                    .then( function(cache) {
+                      cache.put( event.request.url, res.clone() );
+                      return res
+                    } )
+                )
+              } )
+            )
+          }
+        })
+      );
+    }
+  }
+});
+
+self.addEventListener('activate', function(event){
+  console.log('activated!')
+});
---
+++ doc/sw_origin.js
@@ -0,0 +1,39 @@
+// // install
+self.addEventListener('install', function (event) {
+    console.log('sw installed2')
+    event.waitUntil(
+      caches.open('KfsysccCard').then(function(cache) {
+        return cache.addAll([
+          './',
+        ]);
+      })
+    );
+});
+
+// fetch
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+      caches.match(event.request).then( function (response){
+        if (response) {
+          return response;
+        } else{
+          return (
+            fetch(event.request)
+            .then( function(res){
+              return (
+                caches.open( 'KfsysccCard' )
+                  .then( function(cache) {
+                    cache.put( event.request.url, res.clone() );
+                    return res
+                  } )
+              )
+            } )
+          )
+        }
+      })
+    );
+});
+
+self.addEventListener('activate', function(event){
+  console.log('activated!')
+});
